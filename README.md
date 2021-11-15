a7981f6758ae23c9702edb3d77d0a86d49686cf1
Chris Hunley <cshunley21@gmail.com>
2021-11-15 08:34:39 -0600

    making a new Respository

---
+++ README.md
@@ -0,0 +1,21 @@
+# Team9Collab1
+
+First Collaboration for Commonwealth Coders Team 9 using JavaScript, Git, Trello, ChromeDev Tools
+
+Team Nine are: (name: account)
+
+Team membership: GitHub account
+
+Ashley	Gutierrez: ashrhea1986
+
+Christopher	Hunley: cman0493 
+
+Ruth Gough: ruthegough5
+
+Amber Jackson:
+
+We are creating a web-friendly API with JavaScript, Git, Trello, ChromeDev, and Slack.
+
+Christopher Hunley, created and invited team to Trello on 11/4/21
+
+Ruth Gough, created repository and invited team to GitHub Collaboration on 11/4/21
---
+++ script.js
@@ -0,0 +1,48 @@
+function getData() {
+    const response = await fetch('http://hp-api.herokuapp.com/api/character')
+    const data = await response.json()
+  }
+var data = JSON.parse(this.response)
+
+data.forEach(Character => {
+  console.log(Character.name)
+})
+
+var data = JSON.parse(this.response)
+
+if (request.status >= 200 && request.status < 400) {
+  data.forEach(movie => {
+    console.log(movie.title)
+  })
+} else {
+  console.log('error')
+}
+const app = document.getElementById('root')
+const character = document.createElement('character')
+const container = document.createElement('div')
+container.setAttribute('class', 'container')
+app.appendChild(character)
+app.appendChild(container)
+
+data.forEach(character => {
+    console.log(character.name)
+    console.log(movie.title)
+  })
+
+  data.forEach(character => {
+    const card = document.createElement('div')
+    card.setAttribute('class', 'card')
+    const h1 = document.createElement('h1')
+    h1.textContent = character.name
+    const p = document.createElement('p')
+    character.name = character.name.substring(0, 300)
+    p.textContent = `${character.name}...`
+    container.appendChild(card)
+    card.appendChild(h1)
+    card.appendChild(p)
+  })
+
+const errorMessage = document.createElement('marquee')
+errorMessage.textContent = `Gah, it's not working!`
+app.appendChild(errorMessage)
+
